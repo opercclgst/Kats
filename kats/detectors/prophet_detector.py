db8d8ef9e71b07117a92d2f6dcefc83b9c4cba46
opercclgst <andy@thesensorgroup.com>
2021-07-19 20:35:33 -0700

    Update prophet_detector.py

--- kats/detectors/prophet_detector.py
+++ kats/detectors/prophet_detector.py
@@ -12,8 +12,8 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
-from fbprophet import Prophet
-from fbprophet.serialize import model_from_json, model_to_json
+from prophet import Prophet
+from prophet.serialize import model_from_json, model_to_json
 from kats.consts import TimeSeriesData
 from kats.detectors.detector import DetectorModel
 from kats.detectors.detector_consts import (
